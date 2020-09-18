fc92061659ad27fc70d2a59f741a8000aaa8d3a9
ehab1980 <71230316+ehab1980@users.noreply.github.com>
2020-09-19 02:22:52 +0300

    Add files via upload

---
+++ 3.1.1.12 LAB.py
@@ -0,0 +1,10 @@
+income = float(input("Enter the annual income: "))
+# Put your code here.
+if income <= 85528:
+    tax = (income * 0.18) - 556.02
+    if (income * 0.18) - 556.02< 0:
+        tax =0
+else:
+    tax = 14839 + (0.32 + (income - 85528))
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
